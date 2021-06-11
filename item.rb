0ba30990076ed18f3c269b55fcc42b5abdc02fd6
Gyunbie <gyunbie@gmail.com>
2021-06-11 21:46:03 +0300

    Add 'List' class

--- item.rb
+++ item.rb
@@ -1,7 +1,6 @@
 class Item
   def self.valid_date?(str)
     arr = str.split("-")
-    p arr
 
     if arr[0].size != 4 || !(1..12).include?(arr[1].to_i) || !(1..31).include?(arr[2].to_i)
       return false
@@ -10,8 +9,8 @@ class Item
   end
 
   def initialize(title, deadline, description)
-    @title = title
     raise "Incorrect date." unless Item.valid_date?(deadline)
+    @title = title    
     @deadline = deadline
     @description = description
   end
